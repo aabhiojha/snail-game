3eddc968cdb48b3ab3aaf586fd7cfe5fc238262e
Abhishek Ojha <aabhiojha08@outlook.com>
2025-07-23 04:28:42 -0700

    gravity and keyboard input mechanics implemented

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ snail_rectangle = snail_surface.get_rect(bottomright=(600, 300))
 player_surf = pygame.image.load("graphics\Player\player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 
+player_gravity = -20
+
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -30,12 +33,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if player_rect.collidepoint(event.pos):
+                player_gravity = -20
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player_gravity = -20
+
     # sky and ground background
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
-
-    # pygame.draw.line(screen, "Red", (800, 0), pygame.mouse.get_pos(), 4)
-    pygame.draw.ellipse(screen, "Brown", pygame.Rect(left))
 
     # text score
     pygame.draw.rect(screen, "Pink", score_rectangle)
@@ -50,15 +58,10 @@ while running:
 
     screen.blit(snail_surface, snail_rectangle)
 
-    # Player image
+    # Player
+    player_gravity += 1
+    player_rect.y += player_gravity
     screen.blit(player_surf, player_rect)
-
-    # if player_rect.collidepoint(snail_rectangle):
-    #     print("collision")
-
-    # mouse_pos = pygame.mouse.get_pos()
-    # if player_rect.collidepoint(mouse_pos):
-    #     print(pygame.mouse.get_pressed())
 
     pygame.display.update()
     clock.tick(60)
